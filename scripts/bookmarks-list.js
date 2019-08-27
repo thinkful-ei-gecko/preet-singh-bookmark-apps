5cbe323baa5c446b0b8b9d32cd9c8738b4916fcc
preet-singh <preetsingh13@gmail.com>
2019-08-26 23:39:56 -0500

    updated js script files

--- scripts/bookmarks-list.js
+++ scripts/bookmarks-list.js
@@ -4,7 +4,6 @@
 
 const bookmarkList = (function() {
 
-    //function for iterating through store
     function generateBookmarkElement(item){
         return `
         <li class="js-bookmark-list>
@@ -13,24 +12,18 @@ const bookmarkList = (function() {
         </li>`;
     }
 
-    //need to iterate over previous functions elements to generate string
     function generateBookmarkItemsString(bookmarkList){
 
     }
 
 
 
-
-
-
-    //render to DOM
     function renderBookmarkList(){
-        //console.log('`render` runs');
+        //console.log('`render` ran');
 
     }
 
 
-    //bind event listeners
     function handleBookmarkList(){
 
     }
